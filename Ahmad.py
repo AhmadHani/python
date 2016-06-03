f52fc1d382bb8b49e8f7adae9ab1e89cb82d3d6d
AhmadHani <dr.dp2007@gmail.com>
2016-06-03 15:07:03 +0300

    Python Script

---
+++ Ahmad.py
@@ -0,0 +1,11 @@
+name = input("what's your name")
+age = input("What's Age")
+school = input("Whats the study School")
+
+def all(name,age,school):
+    print("######",name,"########\n")
+    print("######",age,"########")
+    print("######",school,"########")
+
+
+all(name,age,school)
